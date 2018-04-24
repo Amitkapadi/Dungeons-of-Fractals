771a835ef4a6beccfb8f04303f46d4edc98def3f
frizik <tester251100@gmail.com>
2018-04-24 13:11:33 +0300

    24.04.2018

---
+++ Slasher/Assets/Scripts/EnemyAIScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyAIScript : MonoBehaviour
+{
+
+    public GameObject Player;
+    public float Distance;
+    NavMeshAgent nav;
+    public float Radius = 40;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance = Vector3.Distance(Player.transform.position, transform.position);
+        if (Distance > Radius)
+        {
+            nav.enabled = false;
+        }
+        if (Distance < Radius)
+        {
+            nav.enabled = true;
+            nav.SetDestination(Player.transform.position);
+        }
+    }
+}
