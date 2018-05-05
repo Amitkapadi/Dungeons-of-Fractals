74099029bc33efa06e1b2c2c56ae4417227942f2
frizik <tester251100@gmail.com>
2018-05-05 09:46:02 +0300

    05.05.2018

--- Slasher/Assets/Scripts/ProceduralGeneration.cs
+++ Slasher/Assets/Scripts/ProceduralGeneration.cs
@@ -3,55 +3,39 @@ using System.Collections;
 
 public class ProceduralGeneration : MonoBehaviour
 {
-    public int CountOfTurns = 0;//Кол-во поворотов кривой
-    public int CorridorLength = 0;//Переменная для определения длины коррдиров
-    public int X_Coordinate = 0;//Начальная координата x
-    public int Y_Coordinate = 0;//Начальная координата y
-    bool DirectionOfTrun;
+    public Transform SimpleWall;//Для префаба обычной стены вправо
+    public Transform WallOut;//Для префаба стены прохода
+    bool DirectionOfTrun;//Рандомный поворот
+    public int LenghtOfRoom;
+    public int CountOfRooms ;
+
     void Start()
     {
-        CountOfTurns = Random.Range(10, 20);
-        for (int iteration = 0; iteration < CountOfTurns; iteration++)
+        float X_Coordinate = 9;//Начальная координата X
+        float Y_Coordinate = 0;//Начальная координата Y
+        float Z_Coordinate = 2;//Начальная координата Z
+        CountOfRooms = Random.Range(10,15);//Получаем рандомное кол-во комнат
+        Instantiate(SimpleWall, new Vector3(X_Coordinate, Y_Coordinate, Z_Coordinate), Quaternion.identity);
+        for (int i = 0; i < CountOfRooms; i++)
         {
-            CorridorLength = Random.Range(10, 10);
             Random Rand = new Random();
-            DirectionOfTrun = Random.Range(0, 2) == 0 ? false : true;
-            for (int j = 0; j < CorridorLength; j++)
+            DirectionOfTrun = Random.Range(0, 2) == 0 ? false : true;//Рандомный поворот
+            LenghtOfRoom = Random.Range(1,3);//Длина комнаты
+            for (int j = 0; j < LenghtOfRoom; j++)
             {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.transform.position = new Vector3(X_Coordinate,1,Y_Coordinate);
-                switch (DirectionOfTrun)
+                if (DirectionOfTrun == false)
                 {
-                    case true:
-                        X_Coordinate++;
-                        break;
-                    case false:
-                        Y_Coordinate++;
-                        break;
+                    Debug.Log("False");
+                    Instantiate(SimpleWall, new Vector3(X_Coordinate, Y_Coordinate, Z_Coordinate), Quaternion.RotateTowards(transform.rotation, SimpleWall.rotation, 270));
+                    X_Coordinate = X_Coordinate + 9.5f;
+                }
+                else
+                {
+                    Debug.Log("true");
+                    Instantiate(SimpleWall, new Vector3(X_Coordinate, Y_Coordinate, Z_Coordinate),Quaternion.identity);
+                    Z_Coordinate = Z_Coordinate + 9.5f;
                 }
             }
         }
-
-        /*int Length =Random.Range(10,20);//Рандомная длина для комнаты
-        int Width=Random.Range(5,15);//Рандомная ширина для комнаты
-        Debug.Log(Length);Debug.Log(Width);
-        for (int CoordinateY = StartPositionY; CoordinateY < Height; CoordinateY++)
-        {
-            for (int CoordinateX = StartPositionX; CoordinateX < StartPositionX + Length; CoordinateX++)
-            {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                //cube.AddComponent<Rigidbody>();
-                cube.transform.position = new Vector3(CoordinateX, CoordinateY,StartPositionZ);
-            }
-        }
-        for (int CoordinateY = StartPositionY; CoordinateY < Height; CoordinateY++)
-        {
-            for (int CoordinateX = StartPositionX; CoordinateX < StartPositionX + Length; CoordinateX++)
-            {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                //cube.AddComponent<Rigidbody>();
-                cube.transform.position = new Vector3(CoordinateX, CoordinateY, StartPositionZ - Width);
-            }
-        }*/
     }
 }
